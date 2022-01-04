50a7fe6245ec7e8d8571efea447049f95a4ad908
mira-kine <mirak0419@gmail.com>
2022-01-03 16:03:03 -0800

    images rendering correctly, image was nested into Link to click, routes to dogs/:id

--- plan.md
+++ plan.md
@@ -5,19 +5,24 @@
 
 ## Routing:
 
-- [ ] /dogs
-- [ ] /dogs/:id
+- [x] /dogs
+- [x] /dogs/:id
 
 ## App Flow:
 
 ### Dogs Home View
 
+- Dogs
+
+  - [x] useState/useEffect to fetchDogs data
+
 - DogList
-  - [ ] .map out dogList
-- [ ] render dog links
-- [ ] render images for each dog
+  - [x] .map out dogList
+- [x] render dog links
+- [x] render images for each dog
 
 ### Dog View
 
+- [ ] useState/useEffect to fetchDogs by Id
 - [ ] DogDetail component
 - [ ] useParams() to match params by id to render individual dog details
--- src/components/DogsList.js
+++ src/components/DogsList.js
@@ -1,5 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-export default function DogsList() {
-  return <div></div>;
+export default function DogsList({ dogs }) {
+  return (
+    <>
+      <div className="container">
+        <h1 className="title">Adoptable Friends!</h1>
+        <div className="dog-cards-list">
+          {dogs.map((dog) => (
+            <div className="dog-card" key={dog.id}>
+              <h2>Meet {dog.name}</h2>
+              <Link key={dog.id} to={`/dogs/${dog.id}`}>
+                <img src={`https://placedog.net/500?id=${dog.id}`}></img>
+              </Link>
+              <p>
+                {dog.name} is a(n) {dog.age} year old {dog.breed}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
